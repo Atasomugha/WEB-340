55385976f5b00ea8a60730f1588eb76ba92b5d87
Anachebe Asomugha <atasomugha@gmail.com>
2023-01-12 02:31:51 -0500

    week-2 WEB 340 commit

---
+++ week-2/sports-app/index.js
@@ -0,0 +1,7 @@
+/** Title: index.js
+    Author: Professor Krasso
+    Date: 12 January 2023
+    Modified By: Anachebe Asomugha
+    Description: JavaScript file for WEB 340 
+                 Assignment 2.2 - Dependencies                 
+ */
